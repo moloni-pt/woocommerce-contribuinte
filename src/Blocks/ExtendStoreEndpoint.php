b2b65db069aa090456b0895dca5a92b3528ad440
Fábio Correia <fabio.correia@moloni.com>
2024-05-03 12:40:14 +0100

    Improve

--- src/Blocks/ExtendStoreEndpoint.php
+++ src/Blocks/ExtendStoreEndpoint.php
@@ -2,68 +2,56 @@
 
 namespace Checkout\Contribuinte\Blocks;
 
-use Automattic\WooCommerce\StoreApi\StoreApi;
-use Automattic\WooCommerce\StoreApi\Schemas\ExtendSchema;
 use Automattic\WooCommerce\StoreApi\Schemas\V1\CheckoutSchema;
 use Automattic\WooCommerce\StoreApi\Schemas\V1\CartSchema;
 
 class ExtendStoreEndpoint
 {
-    private $extend;
 
     /**
      * Plugin Identifier, unique to each plugin.
      *
      * @var string
      */
-    const IDENTIFIER = 'contribuinte-checkout';
+    private $identifier = 'contribuinte-checkout';
 
     public function init()
     {
-        $this->extend = StoreApi::container()->get(ExtendSchema::class);
         $this->extend_store();
     }
 
     public function extend_store()
     {
-        if (is_callable([$this->extend, 'register_endpoint_data'])) {
-            $this->extend->register_endpoint_data(
-                [
-                    'endpoint' => CheckoutSchema::IDENTIFIER,
-                    'namespace' => self::IDENTIFIER,
-                    'schema_callback' => [$this, 'storeApiSchemaCallback'],
-                    'schema_type' => ARRAY_A,
-                ]
-            );
+        woocommerce_store_api_register_endpoint_data(
+            [
+                'endpoint' => CheckoutSchema::IDENTIFIER,
+                'namespace' => $this->identifier,
+                'schema_callback' => [$this, 'storeApiSchemaCallback'],
+                'schema_type' => ARRAY_A,
+            ]
+        );
 
-            $this->extend->register_endpoint_data(
-                [
-                    'endpoint' => CartSchema::IDENTIFIER,
-                    'namespace' => self::IDENTIFIER,
-                    'schema_callback' => [$this, 'storeApiSchemaCallback'],
-                    'data_callback' => [$this, 'storeApiDataCallback'],
-                    'schema_type' => ARRAY_A,
-                ]
-            );
-        }
+        woocommerce_store_api_register_endpoint_data(
+            [
+                'endpoint' => CartSchema::IDENTIFIER,
+                'namespace' => $this->identifier,
+                'schema_callback' => [$this, 'storeApiSchemaCallback'],
+                'data_callback' => [$this, 'storeApiDataCallback'],
+                'schema_type' => ARRAY_A,
+            ]
+        );
     }
 
     public function storeApiDataCallback()
     {
-        $data = wc()->session->get(self::IDENTIFIER);
+        $customer = wc()->customer;
 
-        $billingVat = isset($data['billingVat']) ? $data['billingVat'] : null;
-
-        if (null === $billingVat) {
-            $customer = wc()->customer;
-
-            if ($customer instanceof \WC_Customer) {
-                $billingVat = $customer->get_meta('billing_vat');
-            }
+        if ($customer instanceof \WC_Customer) {
+            $billingVat = $customer->get_meta('billing_vat');
         }
 
         return [
-            'billingVat' => empty($billingVat) ? '' : $billingVat
+            'billingVat' => empty($billingVat) ? '' : $billingVat,
         ];
     }
 
@@ -77,14 +65,8 @@ class ExtendStoreEndpoint
                 'readonly' => true,
                 'optional' => true,
                 'arg_options' => [
-                    'validate_callback' => function( $value ) {
-                        // todo: check errors here?
-                        /*return new \WP_Error(
-                            'rest_invalid_param',
-                            'asdasdas'
-                        );
-
-                        return false;*/
+                    'validate_callback' => function ($value) {
+                        return true;
                     },
                 ],
             ],
